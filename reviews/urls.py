c1edd59f82076cafa6c173373ca1178423796a11
Cannon-Cole <colecannonschool@gmail.com>
2022-07-24 20:24:47 -0600

    Added csrf token to form and created thank you page for form submission

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path("", views.review),
-     path("thank-you", views.thank_you)
+    path("", views.review),
+    path("thank-you", views.thank_you)
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def review(request):
-  if request.method == 'POST':
-    entered_username = request.POST['username']
-    print(entered_username)
-    return HttpResponseRedirect("/thank-you")
 
-  return render(request, "reviews/review.html")
+def review(request):
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+        return HttpResponseRedirect("thank-you")
+
+    return render(request, "reviews/review.html")
 
 
 def thank_you(request):
-  return render(request, "reviews/thank_you.html")
+    return render(request, "reviews/thank_you.html")
